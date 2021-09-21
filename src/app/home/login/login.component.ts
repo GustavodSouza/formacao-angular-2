a20b5df752d93e870d8723ff6a78293458af66d4
Gustavo de Souza <gustavo.souza@meta.com.br>
2021-09-20 22:29:16 -0300

    Inicialização do curso

---
+++ src/app/home/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  public usuario = '';
+  public senha = '';
+
+  constructor(
+    private authService: AutenticacaoService,
+  ) { }
+
+  public login(): void {
+    this.authService.autenticar(this.usuario, this.senha);
+  }
+
+}
